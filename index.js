581dcab83616482f763a124fe7c79fb131ca8a32
Magesh <luvmagii143@gmail.com>
2020-09-10 23:27:17 +0530

    complete1

--- index.js
+++ index.js
@@ -2,24 +2,43 @@ const express=require('express');
 const app=express();
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
+
 const server=require('http').Server(app);
 const io=require("socket.io")(server);
 var mysql = require('mysql'); 
+const mongoose=require("mongoose");
+const { strict } = require('assert');
 app.use(bodyParser.urlencoded({
     extended: true
   }));
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "saimagesh",
-    database: "vvvsi"
-  });
 
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
-  
+  //mysql
+  // var con = mysql.createConnection({
+  //   host: "localhost",
+  //   user: "root",
+  //   password: "saimagesh",
+  //   database: "vvvsi"
+  // });
+
+  // con.connect(function(err) {
+  //   if (err) throw err;
+  //   console.log("Connected!");
+  // });
+
+  //mongoDB
+
+  mongoose.connect("mongodb+srv://vvvsiuser:vvvsipass@cluster0.lsows.mongodb.net/vvvsi?retryWrites=true&w=majority",{useNewUrlParser: true, useUnifiedTopology: true} );
+  const schema={
+    userid:String,
+    username:String,
+    qualification:String,
+    whatsapp_num:Number,
+    gender:String,
+    district:String,
+    joinstatus:String,
+    dateofapply:Date,
+    dateofgiven:Date};
+    User=mongoose.model("User",schema);
 
 app.use(express.static('public'));
 app.set('view engine','ejs');
@@ -31,28 +50,57 @@ app.get('/home',(req,res)=>{
     res.render('home',{message:''});
 });
 app.post('/joingroup',(req,res)=>{
-    var sql = "INSERT INTO userdetail (userid,username,qualification,whatsapp_num,gender,district,joinstatus,dateofapply,dateofgiven) VALUES ?";  
-    dataofapply=new Date();
+   // var sql = "INSERT INTO userdetail (userid,username,qualification,whatsapp_num,gender,district,joinstatus,dateofapply,dateofgiven) VALUES ?";  
+    dataofapply1=new Date();
     userid1=req.body.whatsapp_num+"#vvvsm";
-    console.log(userid1);
-  var values = [  
-  [userid1,req.body.username,req.body.qualification,req.body.whatsapp_num,req.body.gender,req.body.district,"no",dataofapply,0],  
-  ];
-  var sql2="SELECT * FROM userdetail WHERE userid = ?"
-  var data=req.body.whatsapp_num+"#vvvsm";
-  con.query(sql2,data, function (err, result) {
-    if (err) throw err;
-    if(result.length==0){
-      con.query(sql, [values], function (err, result) {
-        if (err) throw err;
-        res.render("home",{message:"Your data have been recived! Soon we will get you"});
-      });
-    }
-    else{
+  
+    const newuser=new User({
+      userid:userid1,
+     username:req.body.username,
+     qualification:req.body.qualification,
+     whatsapp_num:req.body.whatsapp_num,
+     gender:req.body.gender,
+     district:req.body.district,
+      joinstatus:"no",
+      dateofapply:dataofapply1,
+     dateofgiven:"0"  
+    });
+    User.findOne({userid:userid1},function(err,result){
+      if(err){
+        console.log("not found");
+    
+      }
+      else{
+    //console.log(result.userid,userid1);
+    if(result){
+      if(result.userid==userid1){
         res.render("home",{message:"you have already applied"});
-    
+      }
+    }
+      
+    else{
+      newuser.save();
+      res.render("home",{message:"Your data have been recived! Soon we will get you"});
     }
     
-  });
+  }
+      
+    })
+  // var sql2="SELECT * FROM userdetail WHERE userid = ?"
+  // var data=req.body.whatsapp_num+"#vvvsm";
+  // con.query(sql2,data, function (err, result) {
+  //   if (err) throw err;
+  //   if(result.length==0){
+  //     con.query(sql, [values], function (err, result) {
+  //       if (err) throw err;
+  //       res.render("home",{message:"Your data have been recived! Soon we will get you"});
+  //     });
+  //   }
+  //   else{
+  //       res.render("home",{message:"you have already applied"});
+    
+  //   }
+    
+  // });
 })
  server.listen(process.env.PORT||3030);
